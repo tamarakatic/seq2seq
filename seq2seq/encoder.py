b2ba8c93ad8744d0dbaf695d310f256278edd340
Tamara Katic <tamarakatic@gmail.com>
2018-06-10 00:40:41 +0200

    Update encoder rnn

--- seq2seq/encoder.py
+++ seq2seq/encoder.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 
 
-def encoder_rnn_layer(rnn_inputs, rnn_size, num_layers, keep_prob, sequence_length):
+def encoder_rnn(rnn_inputs, rnn_size, num_layers, keep_prob, sequence_length):
     lstm = tf.contrib.rnn.BasicLSTMCell(rnn_size)
     lstm_dropout = tf.contrib.rnn.DropoutWrapper(lstm, input_keep_prob=keep_prob)
     encoder_cell = tf.contrib.rnn.MultiRNNCell([lstm_dropout] * num_layers)
