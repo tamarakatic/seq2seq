404ea50f4e6d5af1c5797e89df36a71c450d2282
Tamara Katic <tamarakatic@gmail.com>
2018-06-30 15:12:04 +0200

    Prepare data for training

--- seq2seq/data_utils.py
+++ seq2seq/data_utils.py
@@ -1,37 +1,13 @@
-import codecs
-import re
+import nltk
+import itertools
+import numpy as np
+import pickle
 
 import tensorflow as tf
 
-EMOJI = re.compile('[\U00010000-\U0010ffff]', flags=re.UNICODE)
+from preprocess_data import clean_text
 
-
-def read_txt(path):
-    return codecs.open(path, 'r', encoding='utf-8', errors='ignore').read().split('\n')[:-1]
-
-
-def clean_text(lines):
-    clean_lines = []
-    for line in lines:
-        line = line.lower()
-        line = re.sub(r"i'm", "i am", line)
-        line = re.sub(r"he's", "he is", line)
-        line = re.sub(r"she's", "she is", line)
-        line = re.sub(r"that's", "that is", line)
-        line = re.sub(r"what's", "what is", line)
-        line = re.sub(r"where's", "where is", line)
-        line = re.sub(r"how's", "how is", line)
-        line = re.sub(r"\'ll", " will", line)
-        line = re.sub(r"\'ve", " have", line)
-        line = re.sub(r"\'re", " are", line)
-        line = re.sub(r"\'d", " would", line)
-        line = re.sub(r"n't", " not", line)
-        line = re.sub(r"won't", "will not", line)
-        line = re.sub(r"can't", "cannot", line)
-        line = EMOJI.sub(r'', line)
-        line = re.sub(r"[-()\"#/%&$@;:<>*^_{}`+=~|.!?,]", "", line)
-        clean_lines.append(line)
-    return clean_lines
+VOCAB_SIZE = 6000
 
 
 def model_inputs():
@@ -54,97 +30,58 @@ def convert_string_to_int(question, word2int):
     return [word2int.get(word, word2int['<OUT>']) for word in new_question.split()]
 
 
-def form_ques_answ(clean_questions, clean_answers):
-    short_questions = []
-    short_answers = []
-    i = 0
-    for question in clean_questions:
-        if 2 <= len(question.split()) <= 25:
-            short_questions.append(question)
-            short_answers.append(clean_answers[i])
-        i += 1
+def apply_padding(batch_of_sequences, word_to_int, maxlen):
+    pading_idx = []
+    for word in batch_of_sequences:
+        if word in word_to_int:
+            pading_idx.append(word_to_int[word])
+        else:
+            pading_idx.append(word_to_int['pad'])
 
-    clean_questions = []
-    clean_answers = []
-    i = 0
-    for answer in short_answers:
-        if 2 <= len(answer.split()) <= 25:
-            clean_answers.append(answer)
-            clean_questions.append(short_questions[i])
-        i += 1
+    return pading_idx + [0]*(maxlen - len(batch_of_sequences))
 
-    word2count = {}
-    for question in clean_questions:
-        for word in question.split():
-            if word not in word2count:
-                word2count[word] = 1
-            else:
-                word2count[word] += 1
 
-    for answer in clean_answers:
-        for word in answer.split():
-            if word not in word2count:
-                word2count[word] = 1
-            else:
-                word2count[word] += 1
+def prepare_data(lines):
+    filter_data = clean_text(lines)
+    filter_questions, filter_answers = [], []
 
-    threshold = 15
-    questions_words_to_int = {}
-    word_number = 0
-    for word, count in word2count.items():
-        if count >= threshold:
-            questions_words_to_int[word] = word_number
-            word_number += 1
+    for i in range(0, len(filter_data), 2):
+        question_len = len(filter_data[i].split(' '))
+        answer_len = len(filter_data[i+1].split(' '))
+        if question_len >= 2 and question_len <= 25:
+            if answer_len >= 2 and answer_len <= 25:
+                filter_questions.append(filter_data[i])
+                filter_answers.append(filter_data[i+1])
 
-    answers_words_to_int = {}
-    word_number = 0
-    for word, count in word2count.items():
-        if count >= threshold:
-            answers_words_to_int[word] = word_number
-            word_number += 1
+    question_tokenized = [wordlist.split(' ') for wordlist in filter_questions]
+    answer_tokenized = [wordlist.split(' ') for wordlist in filter_answers]
+    tokenized = question_tokenized + answer_tokenized
 
-    tokens = ['<PAD>', '<EOS>', '<OUT>', '<SOS>']
-    for token in tokens:
-        questions_words_to_int[token] = len(questions_words_to_int) + 1
+    frequent_dictionary = nltk.FreqDist(itertools.chain(*tokenized))
+    vocabulary = frequent_dictionary.most_common(VOCAB_SIZE)
 
-    for token in tokens:
-        answers_words_to_int[token] = len(answers_words_to_int) + 1
+    ind2word = ['_'] + ['pad'] + [x[0] for x in vocabulary]
+    word2idx = dict([(w, i) for i, w in enumerate(ind2word)])
 
-    answers_ints_to_word = {w_i: w for w, w_i in answers_words_to_int.items()}
+    data_len = len(question_tokenized)
+    question_idx = np.zeros([data_len, 25], dtype=np.int32)
+    answer_idx = np.zeros([data_len, 25], dtype=np.int32)
 
-    for i in range(len(clean_answers)):
-        clean_answers[i] += ' <EOS>'
+    for i in range(data_len):
+        question_indexes = apply_padding(question_tokenized[i], word2idx, 25)
+        answer_indexes = apply_padding(answer_tokenized[i], word2idx, 25)
 
-    questions_into_int = []
-    for question in clean_questions:
-        ints = []
-        for word in question.split():
-            if word not in questions_words_to_int:
-                ints.append(questions_words_to_int['<OUT>'])
-            else:
-                ints.append(questions_words_to_int[word])
-        questions_into_int.append(ints)
+        question_idx[i] = np.array(question_indexes)
+        answer_idx[i] = np.array(answer_indexes)
 
-    answers_into_int = []
-    for answer in clean_answers:
-        ints = []
-        for word in answer.split():
-            if word not in answers_words_to_int:
-                ints.append(answers_words_to_int['<OUT>'])
-            else:
-                ints.append(answers_words_to_int[word])
-        answers_into_int.append(ints)
+    np.save('question_idx.npy', question_idx)
+    np.save('answer_idx.npy', answer_idx)
 
-    sorted_clean_questions = []
-    sorted_clean_answers = []
-    for length in range(1, 25 + 1):
-        for i in enumerate(questions_into_int):
-            if len(i[1]) == length:
-                sorted_clean_questions.append(questions_into_int[i[0]])
-                sorted_clean_answers.append(answers_into_int[i[0]])
+    metadata = {
+            'word2idx': word2idx,
+            'idx2word': ind2word,
+            'freq_dist': frequent_dictionary
+            }
 
-    return (answers_words_to_int,
-            answers_ints_to_word,
-            questions_words_to_int,
-            sorted_clean_questions,
-            sorted_clean_answers)
+    with open('../data/metadata.pkl', 'wb') as f:
+        pickle.dump(metadata, f)
